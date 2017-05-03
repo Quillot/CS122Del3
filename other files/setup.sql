b939456c53b9990c30c9fe610dd3a5bfaf923e69
Gab De Jesus <quillot2112@gmail.com>
2017-05-03 22:12:33 +0900

    Update setup.sql

--- other files/setup.sql
+++ other files/setup.sql
@@ -1,12 +1,15 @@
+DROP DATABASE del3db;
+CREATE DATABASE del3db;
 USE del3db;
-DROP TABLE orderinfo;
-DROP TABLE content;
+DROP TABLE invite;
 DROP TABLE delivery;
-DROP TABLE agent;
-DROP TABLE customer;
-DROP TABLE recipient;
-DROP TABLE product;
+DROP TABLE content;
+DROP TABLE orderinfo;
 DROP TABLE feature;
+DROP TABLE product;
+DROP TABLE recipient;
+DROP TABLE customer;
+DROP TABLE agent;
 
 CREATE TABLE agent (
 	agent_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
@@ -14,12 +17,13 @@ CREATE TABLE agent (
 );
 
 CREATE TABLE invite (
-	invite_code INT NOT NULL,
-	used BOOLEAN NOT NULL
+	invite_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
+	invite_code INT,
+	used tinyint(1)
 );
 
 CREATE TABLE customer (
-	customer_id INT NOT NULL PRIMARY KEY UNIQUE,
+	customer_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	agent_id INT,
 	street VARCHAR(255),
 	city VARCHAR(255),
@@ -45,19 +49,28 @@ CREATE TABLE product (
 	price FLOAT(2)
 );
 
+CREATE TABLE feature (
+	product_id INT NOT NULL PRIMARY KEY,
+	feature_desc VARCHAR(255),
+	FOREIGN KEY (product_id) REFERENCES product(product_id)
+);
+
 CREATE TABLE orderinfo (
 	order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE, 
-	agent_id INT,
+	agent_id INT NOT NULL,
+	customer_id INT NOT NULL,
 	recipient_id INT,
 	issue_date DATE,
 	issue_time TIME,
 	delivery_date DATE,
-	delivery_time TIME,
+	delivery_time TIME,	
 	FOREIGN KEY (agent_id) REFERENCES agent(agent_id),
+	FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
 	FOREIGN KEY (recipient_id) REFERENCES recipient(recipient_id)
 );
 
 CREATE TABLE content (
+	-- content_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	order_id INT NOT NULL, 	
 	product_id INT NOT NULL, 
 	PRIMARY KEY (order_id, product_id),
@@ -69,10 +82,11 @@ CREATE TABLE content (
 );
 
 CREATE TABLE delivery (
+	-- delivery_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
 	order_id INT NOT NULL,
-	recipient_id INT,
+	recipient_id INT NOT NULL,
 	PRIMARY KEY (order_id, recipient_id),
-	gift BOOLEAN, -- changed
+	-- gift BOOLEAN, 
 	FOREIGN KEY (order_id) REFERENCES orderinfo(order_id),
 	FOREIGN KEY (recipient_id) REFERENCES recipient(recipient_id) -- from name to id
 );
